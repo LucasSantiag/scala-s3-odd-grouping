c8558eb500df7016e22a99aa3bc5da7f78b979b0
LucasSantiag <lucassantiago17.cardoso08@gmail.com>
2023-03-09 14:40:03 -0300

    Doc

--- src/main/scala/com/vigil/functions/IOFunctions.scala
+++ src/main/scala/com/vigil/functions/IOFunctions.scala
@@ -14,8 +14,23 @@ object IOFunctions {
     StructField("value", IntegerType, nullable = true),
   ))
 
+  /**
+   * Provides the specific delimiter for each file type used in the application
+   *
+   * @param fileType indicates if is a TSV or a CSV
+   * @return String delimiter
+   */
   private def getDelimiterByType(fileType: String) = if (fileType == TSV_EXTENSION) "\t" else ","
 
+  /**
+   * Read all files from path based on file type, will ignore custom headers at each file and convert to
+   * key|value columns format
+   *
+   * @param spark SparkSession
+   * @param fileType String indicating file type
+   * @param inputPath S3 path to read
+   * @return
+   */
   def readFileByType(spark: SparkSession, fileType: String, inputPath: String): DataFrame = {
     spark.read.format("csv")
       .option("delimiter", getDelimiterByType(fileType))
@@ -24,6 +39,12 @@ object IOFunctions {
       .load(s"$inputPath/*.${fileType}")
   }
 
+  /**
+   * Write the DataFrame as a TSV File
+   *
+   * @param df DataFrame to write
+   * @param outputPath Path to write
+   */
   def writeTsv(df: DataFrame, outputPath: String): Unit =
     df.write.format("csv")
       .option("header", "true")
@@ -31,21 +52,52 @@ object IOFunctions {
       .mode(SaveMode.Overwrite)
       .save(s"$outputPath")
 
+  /**
+   * Split the String based on CSV or TSV delimiters
+   *
+   * @param l line parameter to apply regex to split
+   * @return Split line as an Array[String]
+   */
   private def splitByDelimiters(l: String): Array[String] =
     l.split("[,\\t]")
 
+  /**
+   * Transform each element to an Option of Integer to just use numeric values
+   *
+   * @param l Array of elements in the RDD line
+   * @return Each element wrapped in Option monad
+   */
   private def transformToIntOption(l: Array[String]) =
     l.map(_.toIntOption)
 
+  /**
+   * Check all lines that are fully not Integers
+   *
+   * @param l Array of element in the RDD line
+   * @return True if all elements are not Integers
+   */
   private def removeRandomStringHeaders(l: Array[Option[Int]]) =
     l.forall(_.nonEmpty)
 
+  /**
+   * Check for Integers Tuple and apply zero to any value Null
+   *
+   * @param l Array of element in the RDD line
+   * @return Returns key and 0 if the value is null/empty and return key and value for filled values
+   */
   private def transformEmptyLineInZero(l: Array[Option[Int]]) =
     l match {
       case Array(Some(key), None) => (key, 0)
       case Array(Some(key), Some(value)) => (key, value)
     }
 
+  /**
+   * Read input path as an RDD, ignoring all random headers and converting any empty value in zero.
+   *
+   * @param sc SparkContext
+   * @param inputPath input path to read file
+   * @return RDD with formatted content
+   */
   def readRDD(sc: SparkContext, inputPath: String): RDD[KeyPair] = {
     sc
       .textFile(s"$inputPath/*")
@@ -55,9 +107,21 @@ object IOFunctions {
       .map(transformEmptyLineInZero)
   }
 
+  /**
+   * Transform Integer Tuple in a string concatenated by \t (tsv delimiter)
+   *
+   * @param pair KeyPair (Int, Int)
+   * @return String concatenated by delimiter
+   */
   private def toTsvFormat(pair: KeyPair) =
     s"${pair._1}\t${pair._2}"
 
+  /**
+   * Write a RDD[(Int, Int)] in a TSV formatted file
+   *
+   * @param rdd RDD[KeyPair] (Int, Int)
+   * @param outputPath output path to write
+   */
   def writeTsvFromRDD(rdd: RDD[KeyPair], outputPath: String): Unit =
     rdd
       .map(toTsvFormat)
